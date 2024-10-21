21d1c7cd0b51104fb8f193eed2a90d2be1ea9413
cheewhoo <cheewhoo@byu.edu>
2024-10-20 23:04:13 -0600

    tried to run createDatabse in main server but says not public

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,13 +1,28 @@
 import chess.*;
 import server.Server;
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
 
-public class Main {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+//
+//        Server server = new Server();
+//        int port = 8080;
+//        server.run(port);
+//    }
+//}
 
+public class Main{
+    public static void main(String[] args) throws DataAccessException {
+        try { DatabaseManager.createDatabase(); } catch (DataAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+        System.out.println("♕ 240 Chess Server");
         Server server = new Server();
-        int port = 8080;
+        int port = server.run(8080);
         server.run(port);
     }
-}
+    }
